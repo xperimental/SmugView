02b600dfacf35a8f04eef50b4ffcbb0b26ec2a84
Robert <xperimental@solidproject.de>
2010-09-07 16:28:56 +0200

    First database definition.

---
+++ src/net/sourcewalker/smugview/data/SmugView.java
@@ -0,0 +1,143 @@
+package net.sourcewalker.smugview.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Defines the data structure used in the database and content provider.
+ * 
+ * @author Xperimental
+ */
+public final class SmugView {
+
+    public static final String AUTHORITY = "net.sourcewalker.smugview";
+
+    private SmugView() {
+    };
+
+    public static final class Album implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.parse("content://"
+                + AUTHORITY + "/albums");
+
+        /**
+         * Content type of a list of albums.
+         */
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/net.sourcewalker.smugview.album";
+
+        /**
+         * Content type of a single album.
+         */
+        public static final String CONTENT_TYPE_ITEM = "vnd.android.cursor.item/net.sourcewalker.smugview.album";
+
+        /**
+         * Table name.
+         */
+        public static final String TABLE = "albums";
+
+        /**
+         * Title column.
+         */
+        public static final String TITLE = "title";
+
+        /**
+         * Description column.
+         */
+        public static final String DESCRIPTION = "description";
+
+        /**
+         * Access key column.
+         */
+        public static final String KEY = "key";
+
+        /**
+         * Timestamp of last modification on server.
+         */
+        public static final String MODIFIED = "modified";
+
+        /**
+         * Column for last sync.
+         */
+        public static final String SYNC = "sync";
+
+        /**
+         * Create statement.
+         */
+        public static final String SCHEMA = "CREATE TABLE " + TABLE + "(" + _ID
+                + " INTEGER PRIMARY KEY," + TITLE + " TEXT," + DESCRIPTION
+                + " TEXT," + KEY + " TEXT, " + MODIFIED + " INTEGER," + SYNC
+                + " TEXT)";
+
+        /**
+         * Default projection containing all columns.
+         */
+        public static final String[] DEFAULT_PROJECTION = new String[] { _ID,
+                TITLE, DESCRIPTION, KEY, MODIFIED, SYNC };
+
+        /**
+         * Default sort order (newest albums first).
+         */
+        public static final String DEFAULT_SORT_ORDER = MODIFIED + " DESC";
+    }
+
+    public static final class Image implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.parse("content://"
+                + AUTHORITY + "/images");
+
+        /**
+         * Content type of a list of albums.
+         */
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/net.sourcewalker.smugview.image";
+
+        /**
+         * Content type of a single album.
+         */
+        public static final String CONTENT_TYPE_ITEM = "vnd.android.cursor.item/net.sourcewalker.smugview.image";
+
+        /**
+         * Table name.
+         */
+        public static final String TABLE = "images";
+
+        public static final String ALBUM_ID = "album";
+
+        public static final String POSITION = "position";
+
+        public static final String FILENAME = "filename";
+
+        public static final String KEY = "key";
+
+        public static final String DESCRIPTION = "description";
+
+        public static final String THUMBNAIL_URL = "thumbUrl";
+
+        public static final String IMAGE_URL = "imgUrl";
+
+        public static final String MODIFIED = "modified";
+
+        /**
+         * Create statement.
+         */
+        public static final String SCHEMA = "CREATE TABLE " + TABLE + "(" + _ID
+                + " INTEGER PRIMARY KEY, " + ALBUM_ID + " INTEGER, " + POSITION
+                + " INTEGER, " + FILENAME + " TEXT, " + KEY + " TEXT, "
+                + DESCRIPTION + " TEXT, " + THUMBNAIL_URL + " TEXT, "
+                + IMAGE_URL + " TEXT, " + MODIFIED + " TEXT)";
+
+        /**
+         * Default projection containing all columns.
+         */
+        public static final String[] DEFAULT_PROJECTION = new String[] { _ID,
+                ALBUM_ID, POSITION, FILENAME, KEY, DESCRIPTION, THUMBNAIL_URL,
+                IMAGE_URL, MODIFIED };
+
+        /**
+         * Default sort order (by position in album).
+         */
+        public static final String DEFAULT_SORT_ORDER = ALBUM_ID + " ASC, "
+                + POSITION + " ASC";
+
+    }
+
+}
---
+++ src/net/sourcewalker/smugview/data/SmugViewDbHelper.java
@@ -0,0 +1,49 @@
+package net.sourcewalker.smugview.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Helper for creating and accessing the database.
+ * 
+ * @author Xperimental
+ */
+public class SmugViewDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "smugview.db";
+    private static final int DATABASE_VERSION = 9;
+
+    public SmugViewDbHelper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite
+     * .SQLiteDatabase)
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SmugView.Album.SCHEMA);
+        db.execSQL(SmugView.Image.SCHEMA);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite
+     * .SQLiteDatabase, int, int)
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w("SmugViewDbHelper", "Upgrading database from " + oldVersion
+                + " to " + newVersion + " erases all data!");
+        db.execSQL("DROP TABLE IF EXISTS " + SmugView.Album.TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + SmugView.Image.TABLE);
+        onCreate(db);
+    }
+
+}
